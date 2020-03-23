d2b6fafae3ee2f71a195125275d247e528d57401
Jed Boyle <jboyle1@pca.ac.uk>
2020-03-23 17:18:30 +0000

    Added html post method and action, in app.js made an action that takes in the body parser and allows the data to be requested via req.body.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // Requirements
 let express = require("express");
+let bodyParser = require("body-parser");
+
 let app = express();
+
+// Body parser
+let urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+// View engine
+app.set("view engine", "ejs");
 
 // Port
 app.listen(3001);
@@ -11,4 +19,12 @@ app.use("/assets", express.static("assets"));
 // Routing
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
+});
+
+// Form post
+
+app.post("/", urlencodedParser, (req, res) => {
+    console.log(req.body);
+    res.render("partials/form-success", {data: req.body});
+
 });
