695cbc6ff5fd74934980b7888962152c9a71312c
Jed Boyle <jboyle1@pca.ac.uk>
2020-03-23 17:42:12 +0000

    Installed fs and required it to app.js. Then using req.body I converted the data to variables then using fs I wrote the data to a txt file called form-data.txt.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Requirements
 let express = require("express");
 let bodyParser = require("body-parser");
+let fs = require("fs");
 
 let app = express();
 
@@ -21,10 +22,25 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-// Form post
+// User data display page
+app.get("/user-data", (req, res) => {
+    res.sendFile(__dirname + "/form-data.txt");
+});
 
+// Form post
 app.post("/", urlencodedParser, (req, res) => {
     console.log(req.body);
     res.render("partials/form-success", {data: req.body});
 
+    let fName = req.body.firstName
+    let lName = req.body.lastName
+    let email = req.body.email
+    let message = req.body.message
+    let newsLetter = req.body.newsLetter
+    
+    fs.appendFileSync('./form-data.txt', "First Name: " + fName + "\n");
+    fs.appendFileSync('./form-data.txt', "Last Name: " + lName + "\n");
+    fs.appendFileSync('./form-data.txt', "Email: " + email + "\n");
+    fs.appendFileSync('./form-data.txt', "Message: " + message + "\n");
+    fs.appendFileSync('./form-data.txt', "Newsletters: " + newsLetter + "\n" + "\n");
 });
